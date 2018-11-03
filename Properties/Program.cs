ebfa9d6c43d1df32623efabf23f303c4f6341956
MaximkaStar <max.martsinkus@mail.ru>
2018-11-04 00:42:25 +0600

    Initial Commit

---
+++ Properties/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rate;
+            WriteLine("Please, enter a rate of solid: ");
+            int.TryParse(ReadLine(), out rate);
+  
+            Random randomizer = new Random();
+            Solid solid = new Solid();
+            int maxRateOfRandom = 100;
+            for(int i = 0; i < rate; i++)
+            {
+                solid[i] = randomizer.Next(maxRateOfRandom);
+                WriteLine("{0}", solid[i]);
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ Properties/Solid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Solid
+    {
+        public int[] solid;
+
+        public Solid()
+        {
+        }
+
+        public Solid(int size)
+        {
+            solid = new int[size];
+        }
+
+      
+        public int this[int index]
+        {
+            get
+            {
+                return solid[index];
+            }
+            set
+            {
+                solid[index] = value * value;
+            }
+        }
+    }
+}
